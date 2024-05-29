7b8230cb4dc35d2904176146d76730361e9f344c
darthjee <darthjee@gmail.com>
2024-05-29 18:35:13 -0300

    Add spec

--- spec/lib/sonezaki/redis_accessor_spec.rb
+++ spec/lib/sonezaki/redis_accessor_spec.rb
@@ -3,6 +3,25 @@
 require 'spec_helper'
 
 describe Sonezaki::RedisAccessor do
-  it 'does something' do
+  subject(:accessor) do
+    described_class.new(key, **options)
+  end
+
+  let(:key) do
+    SecureRandom.hex(10)
+  end
+
+  let(:options) { {} }
+
+  after do
+    Redis.new.delete(key)
+  end
+
+  describe "get" do
+    context "when nothing has been set" do
+      it do
+        expect(accessor.get).to be_nil
+      end
+    end
   end
 end
