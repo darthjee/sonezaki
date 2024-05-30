d7f0ba1c735485535751fc56800ad17ec31b69df
darthjee <darthjee@gmail.com>
2024-05-30 10:55:22 -0300

    Add doc

--- lib/sonezaki/redis_accessor.rb
+++ lib/sonezaki/redis_accessor.rb
@@ -1,17 +1,31 @@
 # frozen_string_literal: true
 
 module Sonezaki
+  # @api public
+  # @author darthjee
+  #
+  # Accessor that readis and write values into Redis
   class RedisAccessor
     attr_reader :key, :type, :ttl
 
     MONTH = 30 * 24 * 3600
 
+    # @param key [Symbol,String] Key used to save the data on redis
+    # @param type [Symbol] Type of convertion when retrieving the data
+    # @param ttl [Integer] Seconds the data will exist before expiration
     def initialize(key, type: :string, ttl: MONTH)
       @key = key
       @type = type
       @ttl = ttl
     end
 
+    # Save the value into redis
+    #
+    # @param value [Object] Object to be saved
+    #
+    # Saving will convert the value as string
+    #
+    # @return [Object] the value itself as it is read from Redis
     def set(value)
       options = { ex: ttl }.compact
       connection.set(key, value, **options)
