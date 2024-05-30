737564c7b72fcfd7f0e85e8d69cc6411612c27fd
darthjee <darthjee@gmail.com>
2024-05-30 11:50:46 -0300

    Add integration spec

---
+++ spec/integration/yard/sonezaki/redis_accessor_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Sonezaki::RedisAccessor do
+  describe 'yard' do
+    it "Simple usage" do
+      accessor = Sonezaki::RedisAccessor.new("my_key", type: :integer)
+
+      accessor.set("10")
+
+      expect(accessor.get).to eq(10)
+    end
+  end
+end
